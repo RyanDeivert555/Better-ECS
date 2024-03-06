558f204e4138423ae77aff374502233e5571fc9f
Ryan Deivert <rdeivert@icloud.com>
2024-03-05 17:00:15 -0800

    added example hello world

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,51 @@
+mod app;
 mod component;
 mod entity_builder;
+mod prelude;
 mod query;
+mod scheduler;
+mod system;
 mod tests;
 mod world;
-mod system;
-mod scheduler;
-mod app;
+use crate::prelude::*;
 
-fn main() {}
+make_component! {
+    struct Counter(usize);
+}
+
+struct AddCounter;
+impl StartUpSystem for AddCounter {
+    fn run(&mut self, world: &mut World) -> bool {
+        world.add_resource(Counter(0));
+
+        true
+    }
+}
+
+struct DisplayCount;
+impl System for DisplayCount {
+    fn run(&mut self, world: &mut World) -> bool {
+        let counter = world.get_resource::<Counter>().unwrap();
+        println!("Counter: {}", counter.0);
+
+        true
+    }
+}
+
+struct IncCounter;
+impl System for IncCounter {
+    fn run(&mut self, world: &mut World) -> bool {
+        let mut counter = world.get_resource_mut::<Counter>().unwrap();
+        counter.0 += 1;
+
+        counter.0 < 10
+    }
+}
+
+fn main() {
+    App::new()
+        .add_startup_system(AddCounter)
+        .add_system(DisplayCount)
+        .add_system(IncCounter)
+        .run()
+}
---
+++ src/prelude.rs
@@ -0,0 +1,8 @@
+#![allow(unused)]
+pub use crate::app::*;
+pub use crate::component::*;
+pub use crate::entity_builder::*;
+pub use crate::scheduler::*;
+pub use crate::system::*;
+pub use crate::world::*;
+pub use crate::*;
